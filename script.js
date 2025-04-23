67cc15123d99a0c6ab3d517f8dd20d6b3f4dbfa2
arbin <arbinmain@gmail.com>
2025-04-23 10:18:32 +0000

    feat: Add computerChoice function with random number logic
    Implemented the computerChoice function to randomly return one of the
    three valid choices: "rock", "paper", or "scissors". This uses
    Math.random() and Math.floor() to generate a number from 1 to 3, which
    maps to the three possible game option.
    
    This function will be used in each round to simulate the computer's move
    in the Rock, Paper, Scissors game.

--- script.js
+++ script.js
@@ -16,3 +16,19 @@ function humanChoice(){
     }
     return choice;
 }
+
+function computerChoice(){
+    let choiceNumber = Math.floor(1 + Math.random()*3);
+    console.log(choiceNumber);
+    if(choiceNumber == 1){
+        return "rock";
+    }
+    else if(choiceNumber == 2){
+        return "paper";
+    }
+    else{
+        return "scissors";
+    }
+}
+
+console.log(computerChoice());
