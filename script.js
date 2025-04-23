524be7ae3d65cd14039045eb08ae648f6e012170
arbin <arbinmain@gmail.com>
2025-04-23 11:25:13 +0000

    feat: Implement the final playGame function.
    Implemented the playGame function which loops through playRound 5 times , determines the score and display message based on score.
    
    round variable is used to keep trap of iteration and set termination condition.Both humanChoice and computerChoice are called inside loop so that in each iteration there is new value and they are then passed to playRound function.

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ function humanChoice(){
     let choice="";
     while(1){
         choice = prompt("Enter your choice (rock/paper/scissors):");
-        choice = choice.toLocaleLowerCase();
-        if(choice == "rock" || choice == "paper" || choice == "scissors"){
+        choice = choice.toLowerCase();
+        if((choice == "rock" || choice == "paper" || choice == "scissors")){
             break;
         }
         else{
@@ -30,16 +30,42 @@ function computerChoice(){
     }
 }
 
-function playRound(humanChoice,computerChoice){
+function playRound(humanChoice,computerChoice,round){
     if(humanChoice == computerChoice){
-        console.log("You tied!");
+        console.log("You tied on round "+round+"!");
     }
     else if((humanChoice == "rock" && computerChoice == "scissors") || (humanChoice == "paper" && computerChoice == "rock") || (humanChoice == "scissors" && computerChoice == "paper")){
-        console.log("You won the round!");
+        console.log("You won the round "+ round + "!");
         humanScore++;
     }
     else{
-        console.log("You lost this round!");
+        console.log("You lost the round "+ round + "!");
         computerScore++;
     }
 }
+
+function playGame(){
+
+    let round = 1;
+    while(round<=5){
+        alert("Round "+round);
+        const hChoice = humanChoice();
+        const cChoice = computerChoice();
+        playRound(hChoice,cChoice,round);
+        round++;
+    }
+    console.log("Your Score:"+ humanScore);
+    console.log("Computer Score:"+computerScore);
+    if(humanScore > computerScore){
+
+        console.log("Congratulations, You won!");
+    }
+    else if(humanScore < computerScore){
+        console.log("You lost! Try again next time.");
+    }
+    else{
+        console.log("Its a draw!");
+    }
+}
+
+playGame();
